d04b6dc2e92c4d6fc8e317c3a0dfc15c21b9ef65
Lana Begunova <70295997+lana-20@users.noreply.github.com>
2024-03-07 22:36:50 -0800

    Update sessions_ios.py

--- sessions_ios.py
+++ sessions_ios.py
@@ -2,17 +2,19 @@ from appium import webdriver
 from os import path
 from appium.options.common import AppiumOptions
 
-CUR_DIR = path.dirname(path.abspath(__file__))
-APP = path.join(CUR_DIR, 'TheApp.app.zip')
-APPIUM = 'http://localhost:4723'
+# CUR_DIR = path.dirname(path.abspath(__file__))
+# APP = path.join(CUR_DIR, 'TheApp.app.zip')
+APP = "https://github.com/appium-pro/TheApp/releases/download/v1.12.0/TheApp.app.zip"
+APPIUM = "http://localhost:4723"
 
 CAPS = {
     "platformName": "iOS",
     "appium:options": {
-        "platformVersion": "17.2",
+        "platformVersion": "17.4",
         "deviceName": "iPhone 15 Pro Max",
         "automationName": "XCUITest",
-        "app": APP
+        "app": APP,
+        "showXcodeLog": True
     }
 }
 
